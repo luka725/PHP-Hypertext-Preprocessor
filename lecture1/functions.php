a7f80cf17cc554eb58b07569082bf9672d5cd2e2
LAB321 Student09 <lab321student09@gau.local>
2023-03-07 13:48:37 +0400

    Commiting changes

--- lecture1/functions.php
+++ lecture1/functions.php
@@ -11,13 +11,13 @@ function employee_Massive_Filter($array){
         "TaxAmmount"    => $array["taxammount"],
         "SalaryAmmount" => 0
     );
-    if(!empty($array) && $array["taxtype"] === "precentage" ){
+    if(!empty($array) && $array["taxtype"] === "precentage" ):
         $filtered_array["SalaryAmmount"] = intval($array["salary"]) - ((intval($array["salary"]) * intval($array["taxammount"])) / 100);
         $filtered_array["TaxAmmount"] = (intval($array["salary"]) * intval($array["taxammount"])) / 100;
-    }
-    else{
+    else:
         $filtered_array["SalaryAmmount"] = intval($array["salary"]) - intval($array["taxammount"]);
-    }
+    endif;
+    
     return $filtered_array;
 
 }
@@ -32,48 +32,35 @@ function student_Massive_Filter($array){
         "lec" => $array["lec"],
         "dec"  => $array["dec"],
     );
-    if($array['grade'] >= 97 && $array['grade'] <= 100){
+    if($array['grade'] >= 97 && $array['grade'] <= 100):
         $filtered_array['grade'] = "A+";
-    }
-    else if($array['grade'] >= 93 && $array['grade'] <= 96){
+    elseif($array['grade'] >= 93 && $array['grade'] <= 96):
         $filtered_array['grade'] = "A";
-    }
-    else if($array['grade'] >= 90 && $array['grade'] <= 92){
+    elseif($array['grade'] >= 90 && $array['grade'] <= 92):
         $filtered_array['grade'] = "A-";
-    }
-    else if($array['grade'] >= 87 && $array['grade'] <= 89){
+    elseif($array['grade'] >= 87 && $array['grade'] <= 89):
         $filtered_array['grade'] = "B+";
-    }
-    else if($array['grade'] >= 83 && $array['grade'] <= 86){
+    elseif($array['grade'] >= 83 && $array['grade'] <= 86):
         $filtered_array['grade'] = "B";
-    }
-    else if($array['grade'] >= 80 && $array['grade'] <= 82){
+    elseif($array['grade'] >= 80 && $array['grade'] <= 82):
         $filtered_array['grade'] = "B-";
-    }
-    else if($array['grade'] >= 77 && $array['grade'] <= 79){
+    elseif($array['grade'] >= 77 && $array['grade'] <= 79):
         $filtered_array['grade'] = "C+";
-    }
-    else if($array['grade'] >= 73 && $array['grade'] <= 76){
+    elseif($array['grade'] >= 73 && $array['grade'] <= 76):
         $filtered_array['grade'] = "C";
-    }
-    else if($array['grade'] >= 70 && $array['grade'] <= 82){
+    elseif($array['grade'] >= 70 && $array['grade'] <= 82):
         $filtered_array['grade'] = "C-";
-    }
-    else if($array['grade'] >= 67 && $array['grade'] <= 69){
+    elseif($array['grade'] >= 67 && $array['grade'] <= 69):
         $filtered_array['grade'] = "D+";
-    }
-    else if($array['grade'] >= 63 && $array['grade'] <= 66){
+    elseif($array['grade'] >= 63 && $array['grade'] <= 66):
         $filtered_array['grade'] = "D";
-    }
-    else if($array['grade'] >= 60 && $array['grade'] <= 62){
+    elseif($array['grade'] >= 60 && $array['grade'] <= 62):
         $filtered_array['grade'] = "D-";
-    }
-    else if($array['grade'] >= 0 && $array['grade'] <= 59){
+    elseif($array['grade'] >= 0 && $array['grade'] <= 59):
         $filtered_array['grade'] = "F";
-    }
-    else{
+    else:
         $filtered_array['grade'] = "ERROR";
-    }
+    endif;
     return $filtered_array;
 }
 ?>
--- lecture1/index.php
+++ lecture1/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>homework</title>
     <style>
         body{
             display: flex; 
@@ -65,7 +65,7 @@
     <br><br>
     <h3>დავალება 3</h3>
     <br>
-    <form class="task3form" method="get" action="quiz.php">
+    <form class="task3form" method="post" action="quiz.php">
     <?php   
         $quiz_details = file_get_contents("quiz_details.json");
         $res = json_decode($quiz_details);
